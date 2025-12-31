9984f3b5cb1f1ed04fb5d199cde3ff452ce730d7
Raj Tamang <raaj08236@gmail.com>
2025-12-31 19:06:23 +0545

    Statement and comments

--- RPS.java
+++ RPS.java
@@ -16,38 +16,36 @@ class RBS {
         System.out.print("Choose Rock, Paper or Scissor(R,P,S): ");
         char choice = sc.next().toUpperCase().charAt(0);
         
+        System.out.println("Computer choose: "+ computer);
+
+        
         if(choice == computer){
-            System.out.println("Computer choose: "+ computer);
             System.out.println("Its a draw");
         }
         //for Rock
         else if(choice == 'R'){
             if(computer== 'P'){
-                System.out.println("Computer choose: "+ computer);
                 System.out.println("The computer won!");
             }
             else if(computer== 'S'){
-                System.out.println("Computer choose: "+ computer);
                 System.out.println("You won!");
             }
         }
+        //for Paper
         else if(choice == 'P'){
             if(computer== 'S'){
-                System.out.println("Computer choose: "+ computer);
                 System.out.println("The computer won!");
             }
             else if(computer== 'R'){
-                System.out.println("Computer choose: "+ computer);
                 System.out.println("You won!");
             }
         }
+        //for Scissor
         else if (choice == 'S'){
-            if(computer== 'P'){
-                System.out.println("Computer choose: "+ computer);
+            if(computer== 'P'){                
                 System.out.println("The computer won!");
             }
             else if(computer== 'R'){
-                System.out.println("Computer choose: "+ computer);
                 System.out.println("You won!");
             }
         }
